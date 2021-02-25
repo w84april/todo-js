47e11d542769ed7e9fda5c59b4fb2669b2d6e62a
ZelenovSS <zelenovss666@gmail.com>
2021-02-25 17:54:02 +0300

    start version

---
+++ app.js
@@ -0,0 +1,22 @@
+let todos = [];
+
+input.addEventListener("keydown", (e) => {
+  let val = document.getElementById("input").value;
+  if (e.key == "Enter") {
+    if (val.trim() !== "") {
+      todos.push(val.trim());
+      console.log(todos);
+      document.getElementById("input").value = "";
+
+      let listItem = document.createElement("li");
+
+      let deleteIcon = document.createElement("img");
+      deleteIcon.src = "img/deleteIcon.png";
+
+      listItem.innerHTML = "<div>" + val + " </div> <img>";
+      ul.append(listItem, deleteIcon);
+    } else {
+      alert("Поле пусто");
+    }
+  }
+});
