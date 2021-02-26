6db57c78c8e1df7695a180758cb5e0c4e26d954d
= <donyaschefer@gmail.com>
2021-02-26 18:09:31 +0300

    checkbox + delete

--- app.js
+++ app.js
@@ -1,22 +1,92 @@
 let todos = [];
+let countLeft = 0;
+const input = document.querySelector(".input");
+const filterOptions = document.querySelector(".buttons");
+const todoList = document.querySelector(".todo_list");
+const todosLeft = document.querySelector(".todos_left");
+
+filterOptions.addEventListener("click", filterTodos);
 
 input.addEventListener("keydown", (e) => {
-  let val = document.getElementById("input").value;
+  let val = e.target.value;
   if (e.key == "Enter") {
     if (val.trim() !== "") {
       todos.push(val.trim());
       console.log(todos);
-      document.getElementById("input").value = "";
+      e.target.value = "";
 
       let listItem = document.createElement("li");
+      listItem.className = "listitem";
+
+      let message = document.createElement("div");
+      message.innerText = val;
 
       let deleteIcon = document.createElement("img");
       deleteIcon.src = "img/deleteIcon.png";
+      deleteIcon.className = "delete_icon";
 
-      listItem.innerHTML = "<div>" + val + " </div> <img>";
+      let checkBox = document.createElement("input");
+      checkBox.type = "checkbox";
+
+      deleteIcon.addEventListener("click", (event) => {
+        let elem = event.target.closest(".listitem");
+        elem.remove();
+      });
+
+      checkBox.addEventListener("click", (event) => {
+        let elem = event.target.closest(".listitem");
+        elem.classList.toggle("completed");
+      });
+
+      todosLeft.innerHTML = "Tasks left: " + todos.length;
+
+      listItem.appendChild(checkBox);
+      listItem.appendChild(message);
+      listItem.appendChild(deleteIcon);
+
       ul.append(listItem);
     } else {
       alert("Поле пусто");
     }
   }
 });
+
+// с помощью toggle работало криво, так норм
+function filterTodos(event) {
+  todos = todoList.querySelectorAll("li");
+  console.log(todos);
+  todos.forEach((todo) => {
+    console.log(todo.classList);
+    switch (event.target.innerText) {
+      case "All":
+        todo.style.display = "flex";
+        break;
+      case "Active":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "none";
+          break;
+        } else {
+          todo.style.display = "flex";
+          break;
+        }
+      case "Done":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+          break;
+        } else {
+          todo.style.display = "none";
+          break;
+        }
+    }
+  });
+}
+
+function countTodosLeft(todos) {
+  let count = 0;
+  todos.forEach((todo) => {
+    if (todo.classList.contains("completed")) {
+      count++;
+    }
+  });
+  return count;
+}
